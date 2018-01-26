58f98124535e1c32bb52dfbd56f4a15553e175d5
Kidd <HEDE0001@E.NTU.EDU.SG>
2018-01-26 11:56:17 +0800

    6_%exercise
    6_%exercise

---
+++ 6_%exercise.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+g1 = float(input('Please insert XiaoMing\'s GPA in 2016:'))
+g2 = float(input('Please insert XiaoMing\'s GPA in 2017:'))
+g = (g2-g1)/g1*100
+print('XiaoMing\'s GPA growth between 2016 and 2017 is %.1f %%' % g)
