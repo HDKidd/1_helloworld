6d8fcb52aead028772df79f0d513edfb54ad7284
Kidd <HEDE0001@E.NTU.EDU.SG>
2018-01-26 14:07:01 +0800

    Create 012_def.py

---
+++ 012_def.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+
+def my_abs(x):
+    if not isinstance(x, (int, float)):
+        raise TypeError('bad operand type')
+    if x >= 0:
+        return x
+    else:
+        return -x
+
+
+a = (input('Please input a figure:', ))
+print(my_abs(a))
